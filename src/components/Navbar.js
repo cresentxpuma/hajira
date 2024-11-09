791d369eba57e375b1589f8b964c5956fd9ecb6e
anwaaransari12 <anwaarahmed0212@gmail.com>
2024-11-09 12:29:42 +0530

    test

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+// import {Nav} from 'react-bootstrap';
+
+function NavigationBar() {
+  return (
+    <>
+     <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+    </>
+  );
+}
+
+export default NavigationBar;
