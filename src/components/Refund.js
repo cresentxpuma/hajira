a9c65ebd5a954f7779ff58cad29cb7e0f42c0a79
anwaaransari12 <anwaarahmed0212@gmail.com>
2024-11-10 10:46:45 +0530

    added footer section

--- src/components/Refund.js
+++ src/components/Refund.js
@@ -13,7 +13,7 @@ const Refund = () => {
                 <li>A non-refundable advance of ₹700 is required to confirm your booking. Cancellations made within 72 hours of the event will receive a partial refund of ₹500. No refunds are available for cancellations after this window.</li>
                 <li>Rescheduling is allowed at no extra charge if requested at least 5 days before the event. Late requests may incur a fee based on availability.</li>
                 <li>No refunds will be provided in case of a no-show. Please ensure timely arrival to make the most of your event.</li>
-                <li>Refunds will be processed within 7 business days and credited to the original payment method. For any questions or to request a cancellation, please contact us via WhatsApp at <strong>9948954545</strong>.</li>
+                <li>Refunds will be processed within 7 business days and credited to the original payment method. For any questions or to request a cancellation, please contact us via WhatsApp at <strong>9999999999</strong>.</li>
             </ul>
 
             <p className="text-xl mt-6">
